cbbb27cd9e0da279472e49d47c8e833463bfeb7e
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-03-31 04:22:16 +0600

    Basic Site Cart create

--- src/components/Blogs/Sitecart/Sitecart.jsx
+++ src/components/Blogs/Sitecart/Sitecart.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Sitecart = () => {
   return (
-    <div>
-      
+    <div className='site-cart'>
+      <div className='flex items-center justify-center h-20 bg-slate-200 rounded-lg border border-solid border-blue-600 mb-6'>
+        <p className='text-blue-600 font-bold	text-2xl'>Spent time on read :  min</p>
+      </div>
+      <div className='p-7 bg-slate-200 rounded-lg font-bold	text-2xl text-black'>
+        <p>Bookmarked Blogs : </p>
+      </div>
     </div>
   );
 };
