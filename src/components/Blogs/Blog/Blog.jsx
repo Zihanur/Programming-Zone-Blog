fcb6d3c5452839ca2feb16f4ff1ecc922b11b3f0
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-03-31 14:33:13 +0600

    Added total read time handeler

--- src/components/Blogs/Blog/Blog.jsx
+++ src/components/Blogs/Blog/Blog.jsx
@@ -1,28 +1,42 @@
-import React from 'react';
-import './Blog.css';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
+import "./Blog.css";
 
-const Blog = ({blog}) => {
-  const {id,authorName,coverImg,title,authorImg,readTime} = blog;
+const Blog = ({ blog , readTimeHandel}) => {
+  const { id, authorName, coverImg, title, authorImg, readTime } = blog;
   return (
-    <div className='single-blog'>
-      <img className='w-full h-96 rounded-lg' src={coverImg} alt="" />
-      <div className='w-full flex items-center justify-between my-8'>
-        <div className='flex items-center justify-between'>
+    <div className="single-blog">
+      <img className="w-full h-96 rounded-lg" src={coverImg} alt="" />
+      <div className="w-full flex items-center justify-between my-8">
+        <div className="flex items-center justify-between">
           <div>
-            <img className=' w-14 h-14 rounded-full mr-6' src={authorImg} alt="" />  
+            <img
+              className=" w-14 h-14 rounded-full mr-6"
+              src={authorImg}
+              alt=""
+            />
           </div>
           <div>
-            <h4 className='font-bold text-2xl'>{authorName}</h4>
-            <p className='publish-date font-semibold text-md'>Mar 14 (4 Days ago)</p>
+            <h4 className="font-bold text-2xl">{authorName}</h4>
+            <p className="publish-date font-semibold text-md">
+              Mar 14 (4 Days ago)
+            </p>
           </div>
         </div>
         <div>
-            <p className='read-length text-xl font-medium'>{readTime} min read</p>
-          </div>
+          <p className="read-length text-xl font-medium">
+            {readTime} min read
+            <button onClick={()=>addBookmarkHandel(title)}  className="hover:text-neutral-800"><FontAwesomeIcon className="w-6 h-6 ms-2" icon={faBookmark} />
+            </button>
+          </p>
+        </div>
       </div>
-      <h2 className='font-bold	text-4xl mb-4'>{title}</h2>
-      <p className='font-semibold	text-xl	text-blue-600 underline mb-9'>Mark as read</p>
-      <hr className='hr mb-10'/>
+      <h2 className="font-bold	text-4xl mb-4">{title}</h2>
+      <button className="font-semibold	text-xl	text-blue-600 underline mb-9 pointer-events-auto hover:text-orange-300 " onClick={()=>readTimeHandel(readTime)}>
+        Mark as read
+      </button>
+      <hr className="hr mb-10" />
     </div>
   );
 };
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -5,6 +5,7 @@ import Sitecart from './Sitecart/Sitecart';
 
 const Blogs = () => {
   const [blogs,setBlogs] = useState([]);
+  const [totalTime, setTotalTime] = useState(0);
 
   useEffect(()=>{
     fetch('knowledge.json')
@@ -12,15 +13,27 @@ const Blogs = () => {
     .then(data=>setBlogs(data))
   },[])
 
+  
+  const readTimeHandel = (readTime) =>{
+    const totalReadTime = totalTime + readTime
+    setTotalTime(totalReadTime);
+  }
+
   return (
     <div className='mx-28 grid gap-x-6 sl:grid-cols-1 md:grid-cols-5'>
       <div className='col-span-3'>
         {
-          blogs.map(blog=><Blog blog={blog}></Blog>)
+          blogs.map(blog=>
+          <Blog 
+          key = {blog.id}
+          blog = {blog} 
+          readTimeHandel = {readTimeHandel}
+          ></Blog>)
         }
       </div>
       <div className='col-span-2'>
-        <Sitecart></Sitecart>
+        
+        <Sitecart totalTime={totalTime}></Sitecart>
       </div>
     </div>
   );
